799cc140b9e0b6e76688e1a6360a96935318cbb4
divyanshu <divyanshu.1822en1171@kiet.edu>
2025-10-10 10:46:19 +0530

    Vertical traversal

---
+++ Tree/Oct11_25/VerticalTraversalBFS.java
@@ -0,0 +1,46 @@
+package Oct11_25;
+import java.util.*;
+class Node {
+    int data;
+    Node left, right;
+
+    Node(int val) {
+        data = val;
+        left = right = null;
+    }
+}
+class Pair{
+    Node node;
+    int level;
+    Pair(Node node, int level){
+        this.node = node;
+        this.level = level;
+    }
+}
+
+class Solution {
+    
+    public ArrayList<ArrayList<Integer>> verticalOrder(Node root) {
+        // idea is here we wanted sorted in level order along with vertical order
+        // so if we just have to print vertical order in any order we could have use DFS
+        // but here we need data in ascending level order
+        // so instead sorting the resultant map
+        // just traverse in level order we will autmatically get sorted level order data
+
+
+        if (root == null) return new ArrayList<>();
+        Map<Integer, ArrayList<Integer>> mp = new TreeMap<>();
+        Queue<Pair> q  = new LinkedList<>();
+        q.offer(new Pair(root, 0));
+        while(!q.isEmpty()){
+            Pair curr = q.poll();
+            Node node = curr.node;
+            int level = curr.level;
+            mp.computeIfAbsent(level, k->new ArrayList<>()).add(node.data);
+            if(node.left!=null)q.offer(new Pair(node.left, level-1));
+            if(node.right!=null)q.offer(new Pair(node.right, level+1));
+        }
+        return new ArrayList<>(mp.values());
+    }
+
+}
---
+++ Tree/Oct11_25/VerticalTraversalDFS.java
@@ -0,0 +1,51 @@
+package Oct11_25;
+
+import java.util.*;
+import java.util.stream.Collectors;
+class Node {
+    int data;
+    Node left, right;
+
+    Node(int val) {
+        data = val;
+        left = right = null;
+    }
+}
+class Pair{
+    int val;
+    int level;
+    Pair(int val, int level){
+        this.val = val;
+        this.level = level;
+    }
+}
+
+class Solution {
+    Map<Integer, ArrayList<Pair>> mp = new HashMap<>();
+    public ArrayList<ArrayList<Integer>> verticalOrder(Node root) {
+        mp.clear();
+        int val = 0, level=0;
+        helper(root, val, level);
+        ArrayList<ArrayList<Integer>> res = new ArrayList<>();
+        List<Integer> sortedKeys = mp.keySet()
+                                    .stream()
+                                    .sorted()
+                                    .collect(Collectors.toList());
+        for(int i=0; i<mp.size(); i++){
+            ArrayList<Pair> temp = mp.get(sortedKeys.get(i));
+            temp.sort(Comparator.comparingInt((Pair p) -> p.level));
+            ArrayList<Integer> fin = new ArrayList<>();
+            for(int j=0;j<temp.size(); j++){
+                fin.add(temp.get(j).val);
+            }
+            res.add(fin);
+        }
+        return res;
+    }
+    void helper(Node root, int val, int level){
+        if(root==null)return;
+        mp.computeIfAbsent(val, k -> new ArrayList<>()).add(new Pair(root.data, level));
+        helper(root.left, val-1, level+1);
+        helper(root.right, val+1, level+1);
+    }
+}
