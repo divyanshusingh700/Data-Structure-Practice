1d071f2bf727a7fca317dbf12e63ca1953fa6fcf
divyanshu <divyanshu.1822en1171@kiet.edu>
2025-10-19 21:16:04 +0530

    ConstructBSTFromPreorder O(N) approach

--- Tree/Oct19_25/ConstructBSTFromPreorder.java
+++ Tree/Oct19_25/ConstructBSTFromPreorder.java
@@ -18,23 +18,36 @@ public class ConstructBSTFromPreorder {
 
         for (int i = 1; i < pre.length; i++) {
             Node temp = null;
-
-            // Keep popping until we find the parent to attach current element as right child
             while (!st.isEmpty() && pre[i] > st.peek().data) {
                 temp = st.pop();
             }
 
             if (temp != null) {
-                // If popped, this is the right child
                 temp.right = new Node(pre[i]);
                 st.push(temp.right);
             } else {
-                // Else, this becomes left child of st.peek()
                 st.peek().left = new Node(pre[i]);
                 st.push(st.peek().left);
             }
         }
 
+        return root;
+    }
+
+
+
+    static int idx = 0;
+    public static Node constructUtil(List<Integer> pre, 
+                                   int min, int max) {
+        if (idx >= pre.size())
+            return null;
+        int key = pre.get(idx);
+        if (key <= min || key >= max)
+            return null;
+        Node root = new Node(key);
+        idx++;
+        root.left = constructUtil(pre, min, key);
+        root.right = constructUtil(pre, key, max);
         return root;
     }
 }
