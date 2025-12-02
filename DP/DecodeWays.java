609d31fa01928dfb5a1886d36c88d64d67235bc1
divyanshu <divyanshu.1822en1171@kiet.edu>
2025-12-03 00:10:08 +0530

    Decode ways DP

--- DP/DecodeWays.java
+++ DP/DecodeWays.java
@@ -25,3 +25,25 @@ class Solution {
         return memo[i] = res;
     }
 }
+
+class SolutionDP {
+    
+    public int numDecodings(String s) {
+        int n = s.length();
+        int[] dp = new int[n + 1];
+        dp[n] = 1;
+
+        for(int i = n - 1; i > -1; i--){
+            if(s.charAt(i) == '0')dp[i] = 0;
+            else{
+                dp[i] = dp[i + 1];
+                if(i + 1 < n){
+                    int two = Integer.parseInt(s.substring(i, i + 2));
+                    if(two <= 26)dp[i] += dp[i + 2];
+                }
+            }
+        }
+
+        return dp[0];
+    }
+}
