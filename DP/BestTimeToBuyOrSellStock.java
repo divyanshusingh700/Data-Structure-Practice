9775130a8961be8a04d41d9491e1000f0c8cc707
divyanshu <divyanshu.1822en1171@kiet.edu>
2025-12-09 00:16:13 +0530

    Best Time to Buy and Sell Stock with Cooldown

--- DP/BestTimeToBuyOrSellStock.java
+++ DP/BestTimeToBuyOrSellStock.java
@@ -13,3 +13,31 @@ class Solution {
         }return memo[idx][buy] = Math.max(prices[idx] + dfs(prices, idx + 2, 1), dfs(prices, idx + 1, 0));// either sell or skip
     }
 }
+// bottom up
+class BottomUpDP {
+    public int maxProfit(int[] prices) {
+        int[][] dp = new int[prices.length + 2][2];
+        for(int i = prices.length - 1; i > -1; i--){
+            for(int buy = 0; buy < 2; buy++){
+                if(buy == 1){
+                     dp[i][buy] = Math.max(-prices[i] + dp[i + 1][0], dp[i + 1][1]);// either buy or skip
+                }
+                else dp[i][buy] = Math.max(prices[i] + dp[i + 2][1], dp[i + 1][0]);// either sell or skip
+
+            }
+        }
+        return dp[0][1];
+    }
+}
+
+// bottom up
+class BU {
+    public int maxProfit(int[] prices) {
+        int[][] dp = new int[prices.length + 2][2];
+        for(int i = prices.length - 1; i > -1; i--){
+            dp[i][1] = Math.max(-prices[i] + dp[i + 1][0], dp[i + 1][1]);// either buy or skip
+            dp[i][0] = Math.max(prices[i] + dp[i + 2][1], dp[i + 1][0]);// either sell or skip
+        }
+        return dp[0][1];
+    }
+}
